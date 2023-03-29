6b6b737342daffc21cd743362ac0b420efc89a4a
Amadu <amaduhamzajaarah300@gmail.com>
2023-03-28 18:25:06 -0700

    made some changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,7 @@ include stdlib
 
 file_line { 'SSH Private Key':
   path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
+  line               => '    IdentityFile ~/.ssh/id_rsa',
   match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
   replace            => true,
   append_on_no_match => true
